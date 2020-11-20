0a8a68bee7a32d319a067e01517c6807a86363b8
Ivan Joshua Punzalan Arsua <iparsua@csmbataan.ph.education>
2020-11-19 16:25:02 -0800

    Adding button click event

--- IvanJoshuaArsua.WeatherPanel/IvanJoshuaArsua.WeatherPanel.Windows/MainWindow.xaml.cs
+++ IvanJoshuaArsua.WeatherPanel/IvanJoshuaArsua.WeatherPanel.Windows/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace IvanJoshuaArsua.WeatherPanel.Windows
         {
             InitializeComponent();
         }
+
+        private void btnGetWeather_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
