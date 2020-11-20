6add2eb3e861a06106b43f822997430643b553fb
Ivan Joshua Punzalan Arsua <iparsua@csmbataan.ph.education>
2020-11-19 17:04:57 -0800

    Creating CurrentWeather class and adding a CurrentWeather nested property to WeatherArea class

--- IvanJoshuaArsua.WeatherPanel/IvanJoshuaArsua.WeatherPanel.Windows/WeatherArea.cs
+++ IvanJoshuaArsua.WeatherPanel/IvanJoshuaArsua.WeatherPanel.Windows/WeatherArea.cs
@@ -11,5 +11,13 @@ namespace IvanJoshuaArsua.WeatherPanel.Windows
         public string Latitude { get; set; }
 
         public string Longitude { get; set; }
+
+        public CurrentWeather Currently { get; set; }
+    }
+    public class CurrentWeather
+    {
+        public string Summary { get; set; }
+
+        public string Icon { get; set; }
     }
 }
